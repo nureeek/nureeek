ab568cf4b47d37d0b65f501ad301b2a12f932102
nureeek <241187@astanait.edu.kz>
2024-12-21 10:30:43 +0500

    Created nureeek class and its subclasses

---
+++ src/bird.java
@@ -0,0 +1,16 @@
+public class bird extends nureeek {
+    private String something;
+
+    public bird(){
+        super();
+    }
+    public Bird(String name, int age,Boolean gender, String something)
+
+    public String getSomething() {
+        return something;
+    }
+
+    public void setSomething(String something) {
+        this.something = something;
+    }
+}
